eb40d3ac0a937f38ef78ac07bd7b76e771afa5a2
ayushsubedi <ayush.subedi@gmail.com>
2024-07-15 10:45:12 +0545

    remove all errors

--- js/main.js
+++ js/main.js
@@ -111,38 +111,6 @@
     }
   });
   document.addEventListener("DOMContentLoaded", function() {
-    const scrollContainer = document.getElementById("slider-container");
-    const halfwayScroll = (scrollContainer.scrollWidth - scrollContainer.clientWidth) / 2;
-    scrollContainer.scrollLeft = halfwayScroll;
-    let isDown = false;
-    let startX;
-    let scrollLeft;
-    scrollContainer.addEventListener("mousedown", (e) => {
-      isDown = true;
-      scrollContainer.classList.add("active");
-      startX = e.pageX - scrollContainer.offsetLeft;
-      scrollLeft = scrollContainer.scrollLeft;
-      scrollContainer.style.cursor = "grabbing";
-    });
-    scrollContainer.addEventListener("mouseleave", () => {
-      isDown = false;
-      scrollContainer.classList.remove("active");
-      scrollContainer.style.cursor = "grab";
-    });
-    scrollContainer.addEventListener("mouseup", () => {
-      isDown = false;
-      scrollContainer.classList.remove("active");
-      scrollContainer.style.cursor = "grab";
-    });
-    scrollContainer.addEventListener("mousemove", (e) => {
-      if (!isDown) return;
-      e.preventDefault();
-      const x = e.pageX - scrollContainer.offsetLeft;
-      const walk = (x - startX) * 2;
-      scrollContainer.scrollLeft = scrollLeft - walk;
-    });
-  });
-  document.addEventListener("DOMContentLoaded", function() {
     const contactUsButton = document.querySelector("#myLinks-mm #book-a-call");
     if (contactUsButton) {
       contactUsButton.addEventListener("click", function() {
